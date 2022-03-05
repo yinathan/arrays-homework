dff85a341a4d75dd7c7b6203573b726b5df56617
Nathan Yi <nathanyi112@gmail.com>
2022-03-05 01:56:42 -0600

    updated to arrow functions

--- javascript-arrays-homework.js
+++ javascript-arrays-homework.js
@@ -94,95 +94,106 @@ const hasSoup = foods.includes("soup")
 console.log("Exercise 10 Result: ", hasSoup)
 
 /*
-Exercise 11:
-  - Use the forEach method to iterate through the provided nums array and add each odd number to a new array named odds.
-  - Hint: Initialize the odds variable to an empty array before the iteration.
-*/
+// Exercise 11:
+//   - Use the forEach method to iterate through the provided nums array and add each odd number to a new array named odds.
+//   - Hint: Initialize the odds variable to an empty array before the iteration.
+// */
 
-const nums = [100, 5, 23, 15, 21, 72, 9, 45, 66, 7, 81, 90]
+// const nums = [100, 5, 23, 15, 21, 72, 9, 45, 66, 7, 81, 90]
 
-// Complete Exercise 11 below...
-let odds = []
-const oddNums = function(item) {
-    if (item % 2 !=0){
-        odds.push(item)
-    }
-}
-nums.forEach(oddNums)
-console.log("Exercise 11 Result: ", odds)
+// // Complete Exercise 11 below...
+// let odds = []
+// const oddNums = function(item) {
+//     if (item % 2 !=0){
+//         odds.push(item)
+//     }
+// }
+// nums.forEach(oddNums)
+// console.log("Exercise 11 Result: ", odds)
 
-/*
-Exercise 12:
-  - Use the forEach method to iterate through the same nums array and add the number to arrays named fizz, buzz and/or fizzbuzz based upon the following:
-  	- Add to the fizz array if the number is evenly divisible by 3.
-  	- Add to the buzz array if the number is evenly divisible by 5.
-  	- Add to the fizzbuzz array if the number is evenly divisible by 3 & 5.
-*/
+// /*
+// Exercise 12:
+//   - Use the forEach method to iterate through the same nums array and add the number to arrays named fizz, buzz and/or fizzbuzz based upon the following:
+//   	- Add to the fizz array if the number is evenly divisible by 3.
+//   	- Add to the buzz array if the number is evenly divisible by 5.
+//   	- Add to the fizzbuzz array if the number is evenly divisible by 3 & 5.
+// */
 
-// Complete Exercise 12 below...
+// // Complete Exercise 12 below...
 
 
-const fizz = []
-const buzz = []
-const fizzbuzz = []
+// const fizz = []
+// const buzz = []
+// const fizzbuzz = []
 
-const fizzBuzzFinder = function(element) {
-    if (element % 3 === 0 && element % 5 === 0) {
-        fizzbuzz.push(element)
-     } if (element % 5 === 0){
-            buzz.push(element)
-        } if (element % 3 === 0) {
-            fizz.push(element)
-        }
+// const fizzBuzzFinder = function(element) {
+//     if (element % 3 === 0 && element % 5 === 0) {
+//         fizzbuzz.push(element)
+//      } if (element % 5 === 0){
+//             buzz.push(element)
+//         } if (element % 3 === 0) {
+//             fizz.push(element)
+//         }
     
-}
-nums.forEach(fizzBuzzFinder)
+// }
+// nums.forEach(fizzBuzzFinder)
 
-console.log("Exercise 12 Results:")
-console.log("  fizz:", fizz)
-console.log("  buzz:", buzz)
-console.log("  fizzbuzz:", fizzbuzz)
+// console.log("Exercise 12 Results:")
+// console.log("  fizz:", fizz)
+// console.log("  buzz:", buzz)
+// console.log("  fizzbuzz:", fizzbuzz)
 
-/*
-Exercise 13:
-  - Given the below numArrays array of arrays (two-dimensional array), assign the last nested array to a variable named numList.
-  - Assume you don't know how many nested arrays numArrays contains.
-*/
+// /*
+// Exercise 13:
+//   - Given the below numArrays array of arrays (two-dimensional array), assign the last nested array to a variable named numList.
+// //   - Assume you don't know how many nested arrays numArrays contains.
+// // */
 
-const numArrays = [
-  [100, 5, 23],
-  [15, 21, 72, 9],
-  [45, 66],
-  [7, 81, 90],
-]
+// const numArrays = [
+//   [100, 5, 23],
+//   [15, 21, 72, 9],
+//   [45, 66],
+//   [7, 81, 90],
+// ]
 
-// Complete Exercise 13 below...
+// // // Complete Exercise 13 below...
 
-const numList = numArrays[numArrays.length -1]
-console.log("Exercise 13 Result: ", numList)
+// // const numList = numArrays[numArrays.length -1]
+// // console.log("Exercise 13 Result: ", numList)
 
-/*
-Exercise 14:
-  - Given the above numArrays array, access the number 66 and assign to a variable named num.
-*/
+// // /*
+// // Exercise 14:
+// //   - Given the above numArrays array, access the number 66 and assign to a variable named num.
+// // */  
 
-// Complete Exercise 14 below...
-const num = (numArrays[2])[1]
-console.log("Exercise 14 Result: ", num)
+// // // Complete Exercise 14 below...
+// // const num = (numArrays[2])[1]
+// // console.log("Exercise 14 Result: ", num)
 
-/*
-Exercise 15:
-  - Given the above numArrays array, use nested forEach methods to sum up all the numbers contained within numArrays and assign to a variable named total.
-  - Hint: Be sure to declare and initialize the total variable before the iterations.
-*/
+// /*
+// Exercise 15:
+//   - Given the above numArrays array, use nested forEach methods to sum up all the numbers contained within numArrays and assign to a variable named total.
+//   - Hint: Be sure to declare and initialize the total variable before the iterations.
+// */
 
-// Complete Exercise 15 below...
-let total = 0
-const adding = function(element){
-    element.forEach(function(secondElement) {
-        total = total + secondElement
-    })
-}
-numArrays.forEach(adding)
-console.log("Exercise 15 Result: ", total)
+// // Complete Exercise 15 below...
+// let total = 0
+// const adding = function(element){
+//     element.forEach(function(secondElement) {
+//         total = total + secondElement
+//     })
+// }
+
+// let total = 0
+// numArrays.forEach((outer) => {
+//   outer.forEach((inner) => {
+//     total += inner
+//   })
+// })
+
+// // numArrays.forEach(adding)
+// console.log("Exercise 15 Result: ", total)
+
+
+
 
